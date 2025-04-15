9e47b14700346daf29745793562ea0975ff2ae85
jsakoman <josip@sakoman.ch>
2025-04-15 19:05:41 +0200

    manuel prikazi sve iz jsona

--- Vjezbe/vjezba2/src/App.vue
+++ Vjezbe/vjezba2/src/App.vue
@@ -14,6 +14,39 @@ const korisnik = podaci.korisnik
     <p><strong>Adresa:</strong> {{ korisnik.osobni_podaci.adresa.ulica }} {{ korisnik.osobni_podaci.adresa.broj }}, {{ korisnik.osobni_podaci.adresa.grad }}</p>  
     <p><strong>Telefon:</strong> {{ korisnik.osobni_podaci.broj_telefona }}</p>  
   </div>
+
+  <h2 class="text-xl font-bold mb-2">Sadržaj košarice</h2>
+  
+  <!-- JABUKA -->
+  <div class="mb-4">
+    <p><strong>Naziv:</strong> {{ korisnik.kosarica[0].naziv }}</p>
+    <img :src="slike.Jabuka" alt="Jabuka" class="w-10 h-10" />
+    <p><strong>Kolicina:</strong> {{ korisnik.kosarica[0].kolicina }}</p>
+  </div>
+
+    <!-- MRKVA -->
+    <div class="mb-4">
+      <p><strong>Naziv:</strong> {{ korisnik.kosarica[1].naziv }}</p>
+      <img :src="slike.Mrkva" alt="Mrkva" class="w-10 h-10" />
+      <p><strong>Kolicina:</strong> {{ korisnik.kosarica[1].kolicina }}</p>
+    </div>
+
+    <!-- SIR -->
+    <div class="mb-4">
+      <p><strong>Naziv:</strong> {{ korisnik.kosarica[2].naziv }}</p>
+      <img :src="slike.Sir" alt="Sir" class="w-10 h-10" />
+      <p><strong>Kolicina:</strong> {{ korisnik.kosarica[2].kolicina }}</p>
+    </div>    
+
+    <!-- KRUH -->
+    <div class="mb-4">
+      <p><strong>Naziv:</strong> {{ korisnik.kosarica[3].naziv }}</p>
+      <img :src="slike.Kruh" alt="Kruh" class="w-10 h-10" />
+      <p><strong>Kolicina:</strong> {{ korisnik.kosarica[3].kolicina }}</p>
+    </div>    
+
+
+
 </template>
 
 <style scoped>
