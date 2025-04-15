5f66a6fa525186cba029654df549cb4a25567b98
jsakoman <josip@sakoman.ch>
2025-04-15 18:46:18 +0200

    ubacio json i prikazao neke podatke

--- Vjezbe/vjezba2/src/App.vue
+++ Vjezbe/vjezba2/src/App.vue
@@ -1,8 +1,19 @@
 <script setup>
+import podaci from './assets/podaci.json'
+
+const slike = podaci.slike
+const proizvodi = podaci.proizvodi
+const korisnik = podaci.korisnik
 </script>
 
 <template>
-<p>Josip Test</p>
+  <div>
+    <p><strong>Ime:</strong> {{ korisnik.osobni_podaci.ime }}</p>  
+    <p><strong>Prezime:</strong> {{ korisnik.osobni_podaci.prezime }}</p>  
+    <p><strong>Adresa:</strong> {{ korisnik.osobni_podaci.adresa.ulica }} {{ korisnik.osobni_podaci.adresa.broj }}, {{ korisnik.osobni_podaci.adresa.grad }}</p>  
+    <p><strong>Telefon:</strong> {{ korisnik.osobni_podaci.broj_telefona }}</p>  
+  </div>
+
 </template>
 
 <style scoped>
