f672790fdae9425241901f36a64d88b78b5179f5
jsakoman <josip@sakoman.ch>
2025-04-15 20:15:30 +0200

    dodana lista kosarice sa buttons (samo mock)

--- Vjezbe/vjezba3/src/App.vue
+++ Vjezbe/vjezba3/src/App.vue
@@ -13,6 +13,28 @@
         <button @click="dodajProizvod" :disabled="!naziv || cijena <= 0" class="bg-blue-500 text-white px-4 py-2  disabled:bg-gray-500">Dodaj artikl</button>
       </div> 
 
+
+      <!-- Kosarica -->
+      <div v-if="kosarica.length === 0" class="text-gray-500"> Kosarica je prazna!</div>
+      <div v-else>
+        <ul>
+          <li v-for="(stavka, index) in kosarica" :key="index" class="mb-4">
+            <div class="flex justify-between items-center border rounded bg-gray-100">
+              <div>
+                <h3 class="p-6"> {{ stavka.naziv }}</h3>
+                <p>Cijena: {{ stavka.cijena }}€</p>
+              </div>
+              <div>
+                <p>Kolicina: {{  stavka.kolicina }}</p>
+                <button @click="promijeniKolicinu(index, -1)" class="bg-red-500 text-white px-2 py-1">-</button>
+                <button @click="promijeniKolicinu(index, 1)" class="bg-green-500 text-white px-2 py-1">+</button>
+                <button @click="ukloniProizvod(index)" class="bg-gray-500 text-white px-4 py-1">Ukloni</button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+
   </div>
 </template>
 
