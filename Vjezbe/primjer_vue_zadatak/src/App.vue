0f321850cc3c57e79fb1745030057fef1e78b9e9
jsakoman <josip@sakoman.ch>
2025-04-15 20:53:36 +0200

    footer gotov

--- Vjezbe/primjer_vue_zadatak/src/App.vue
+++ Vjezbe/primjer_vue_zadatak/src/App.vue
@@ -290,9 +290,7 @@
                 </section>
             </div>
 
-            <footer class="w-full flex items-center justify-center gap-2 bg-[#070F2B]/25 backdrop-blur-sm py-2 px-8 rounded-t-lg">
-                Izradio: <b class="text-indigo-200">Marko Marković</b>
-            </footer>
+            <Footer />
         </div>
     </div>
 </template>
--- Vjezbe/primjer_vue_zadatak/src/components/Footer.vue
+++ Vjezbe/primjer_vue_zadatak/src/components/Footer.vue
@@ -1,4 +1,7 @@
 <template>
+    <footer class="w-full flex items-center justify-center gap-2 bg-[#070F2B]/25 backdrop-blur-sm py-2 px-8 rounded-t-lg">
+        Izradio: <b class="text-indigo-200">Marko Marković</b>
+    </footer>
 </template>
   
 <script setup>
