ab1d2a28d4eac82eb4cae66e55f4c8b416c616df
jsakoman <josip@sakoman.ch>
2025-04-16 19:38:56 +0200

    zadatak1_3

--- projekt_drugog_primjera_kolokvija/src/components/Zadatak_1.vue
+++ projekt_drugog_primjera_kolokvija/src/components/Zadatak_1.vue
@@ -13,11 +13,24 @@ function provjeraOperacije() {
 </script>
 
 <template>
-    <div>
-        ZADATAK 1
+    <div class="flex items-center justify-center bg-gray-100">
+        <div class="p-4 max-w bg-white">
+            <h1 class="text-2xl font-bold">ZADATAK 1</h1>
+
+            <div class="flex flex-col">
+                <label>Broj 1:
+                    <input v-model="broj1" type="number" class="border p-2"/>
+                </label>
+                <label>Broj 2:
+                    <input v-model="broj2" type="number" class="border p-2"/>
+                </label>
+                <label>Operacija (zbrajanje, oduzimanje, mnozenje, dijeljenje):
+                    <input v-model="operacija" type="text" class="border p-2"/>
+                </label>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-
 </style>
