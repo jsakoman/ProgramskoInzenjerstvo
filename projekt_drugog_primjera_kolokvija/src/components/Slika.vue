8bebcced0339fe9715ae803d08a00a300e20c497
jsakoman <josip@sakoman.ch>
2025-04-18 15:23:45 +0200

    zadatak3_4

--- projekt_drugog_primjera_kolokvija/src/components/Slika.vue
+++ projekt_drugog_primjera_kolokvija/src/components/Slika.vue
@@ -1,21 +1,22 @@
 <script setup>
-defineProps(
-    {
-        slikar: String,
-        godina: Number,
-        naslov: String,
-        slika: String
+const props = defineProps({
+  slikar: String,
+  godina: Number,
+  naslov: String,
+  slika: String
 })
+
+function starost() {
+  const now = new Date().getFullYear()
+  return now - props.godina
+}
 </script>
 
 <template>
-    <div>
-        <img :src="slika" height="100"/>
-        <p><strong>Naslov: {{ naslov }}</strong></p>
-        <p><strong>Godina: {{ godina }}</strong></p>
-        <p><strong>Slikar: {{ slikar }}</strong></p>
-    </div>
+  <div>
+    <img :src="props.slika" height="100" />
+    <p><strong>Naslov: {{ props.naslov }}</strong></p>
+    <p><strong>Starost: {{ starost() }} godina</strong></p>
+    <p><strong>Slikar: {{ props.slikar }}</strong></p>
+  </div>
 </template>
-
-<style scoped>
-</style>
