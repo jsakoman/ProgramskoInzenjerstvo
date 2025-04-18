aefc2f2aeab9d4ca2b9c9eb73b86e7bfe24dcabf
jsakoman <josip@sakoman.ch>
2025-04-18 14:48:32 +0200

    zadatak3_3

--- projekt_drugog_primjera_kolokvija/src/components/Slika.vue
+++ projekt_drugog_primjera_kolokvija/src/components/Slika.vue
@@ -9,6 +9,12 @@ defineProps(
 </script>
 
 <template>
+    <div>
+        <img :src="slika" height="100"/>
+        <p><strong>Naslov: {{ naslov }}</strong></p>
+        <p><strong>Godina: {{ godina }}</strong></p>
+        <p><strong>Slikar: {{ slikar }}</strong></p>
+    </div>
 </template>
 
 <style scoped>
