4487322571e3eb4dfd080f46759d2a84cd730f46
jsakoman <josip@sakoman.ch>
2025-04-18 14:35:00 +0200

    dodan css

--- projekt_drugog_primjera_kolokvija/src/components/Zadatak_2.vue
+++ projekt_drugog_primjera_kolokvija/src/components/Zadatak_2.vue
@@ -67,5 +67,13 @@
 </template>
 
 <style scoped>
-
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
 </style>
