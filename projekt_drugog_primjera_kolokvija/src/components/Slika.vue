73ac11f9979ea22a091973c90cb9a1bec753e60b
jsakoman <josip@sakoman.ch>
2025-04-18 14:39:20 +0200

    zadatak3_1

---
+++ projekt_drugog_primjera_kolokvija/src/components/Slika.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(
+    {
+        slikar: String,
+        godina: Number,
+        naslov: String,
+        slika: String
+})
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
